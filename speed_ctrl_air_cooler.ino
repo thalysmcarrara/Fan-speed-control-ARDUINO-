da98fc6dc909ecbc820004ae6b6ea9b752e3ad20
Thalys Matias Carrara <thalysmatiascarrara@gmail.com>
2020-06-18 18:05:00 -0400

    arquivo .ino de controle controle das ventoinhas

---
+++ speed_ctrl_air_cooler.ino
@@ -0,0 +1,57 @@
+#define pwm 5
+#define direction1 8
+#define direction2 9
+#define pot A0
+
+
+
+void setup() {
+  pinMode(pot,INPUT);
+  pinMode(pwm,OUTPUT);
+  pinMode(direction1,OUTPUT);
+  pinMode(direction2,OUTPUT);
+  Serial.begin(9600);
+  
+
+}
+
+void Controle_velocidade(float tensao, int potenciometro){
+  Serial.print("Tensao= ");
+  Serial.println(tensao);
+  //if(tensao < 4.0){
+   digitalWrite(pwm,potenciometro);
+   digitalWrite(direction1,LOW);
+    digitalWrite(direction2,HIGH);
+ // }else{
+    //digitalWrite(pwm,86);
+    //digitalWrite(direction1,LOW);
+   // digitalWrite(direction2,HIGH);
+    //Serial.println("MOTOR EM POTENCIA MÁXIMA");
+    //}
+  }
+
+void loop() {
+ int potenciometro = 0;
+ int duty_cycle = 0;
+ float vout = 0;
+ 
+ potenciometro = analogRead(pot);
+ //Serial.println(potenciometro);
+
+ potenciometro = map(potenciometro, 0, 1023, 0, 255);
+ //Serial.print("potenciometro= ");
+ //Serial.println(potenciometro);
+
+ duty_cycle = map(potenciometro,0,255,0,100);
+ //Serial.print("duty cycle= ");
+ //Serial.println(duty_cycle);
+
+ vout = (duty_cycle/100.0)*5.0;
+ //Serial.print("Vout= ");
+ //Serial.println(vout);
+
+ Controle_velocidade(vout,potenciometro);
+ 
+
+ 
+}
